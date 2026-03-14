84fd6f066ca5ae7b637c19ee87fa466b581eb17f
Shaunrufus <shaunrufus1412@gmail.com>
2026-03-14 19:33:40 +0530

    Remove hardcoded API key

--- modules/brain.py
+++ modules/brain.py
@@ -6,7 +6,7 @@ Handles all intelligent responses + intent detection
 from groq import Groq
 import random
 
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "os.environ.get("GROQ_API_KEY", "")"
 
 client = Groq(api_key=GROQ_API_KEY)
 
